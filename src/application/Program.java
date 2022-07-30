920ac56fe299f3e94bdcba1c0b4692e4e2643ff2
Pedro Leite <contato.pedro77@gmail.com>
2022-07-30 16:13:54 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,11 +32,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Test 3: seller Insert ===");
+		System.out.println("\n=== Test 4: seller Insert ===");
 		Seller newSeller = new Seller(null, "greg", "greg@hotmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 
+		System.out.println("\n=== Test 5: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Weyne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
